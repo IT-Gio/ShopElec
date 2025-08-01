bbe238d007f4bf303d041141503bfc14b83e1d0d
IT-Gio <shalamberidze2008gio@gmail.com>
2025-08-01 21:23:25 +0400

    Add new featues and updated existing files

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'blog',
-    'shop',
-    'accounts',
+    'app1',
+    'app2',
+    'app3',
 ]
+
 
 
 MIDDLEWARE = [
